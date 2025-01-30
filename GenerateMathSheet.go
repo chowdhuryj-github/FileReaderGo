d5b21b731be71a29b006f10d50ce0ec086bebe18
chowdhuryj-github <chowdhuryjawadul@icloud.com>
2025-01-29 20:13:01 -0600

    three issues resolved

--- GenerateMathSheet.go
+++ GenerateMathSheet.go
@@ -4,26 +4,34 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"math/rand"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // function for creating the problem sheet
-func CreateProblemSheet() string {
+func CreateSheets() (string, string) {
 
 	// variable declaration
-	var fileName string
+	var problemFile string
+	var answerFile string
 
 	// debugging message
 	fmt.Println("Creating a Problem Sheet...")
 
 	// ask for user input for a file name
 	fmt.Print("Please enter a file name: ")
-	fmt.Scanln(&fileName)
-	fmt.Println("File Name: ", fileName+".txt"+"\n")
+	fmt.Scanln(&problemFile)
+	fmt.Println("File Name: ", problemFile+".txt"+"\n")
+
+	// generating the name of answer file
+	answerFile = problemFile + "Answer"
 
 	// creating the text file
-	file, err := os.Create(fileName + ".txt")
+	fileOne, err := os.Create(problemFile + ".txt")
+	fileTwo, err := os.Create(answerFile + ".txt")
 
 	// error handling
 	if err != nil {
@@ -31,9 +39,10 @@ func CreateProblemSheet() string {
 	}
 
 	// debugging message
-	fmt.Print("Generated ", file.Name(), " ...")
+	fmt.Print("Generated ", fileOne.Name(), " ...")
+	fmt.Print("Generate ", fileTwo.Name(), " ...")
 
-	return file.Name()
+	return fileOne.Name(), fileTwo.Name()
 
 }
 
@@ -70,9 +79,114 @@ func GenerateRandomOperator() string {
 
 }
 
+// function for performing the math calculation
+func PerformMathOperation(numberOne int, numberTwo int, operator string) int {
+
+	var totalNumber int
+
+	// swithcing between chosen operations
+	switch operator {
+	case "+":
+		totalNumber = numberOne + numberTwo
+	case "-":
+		totalNumber = numberOne - numberTwo
+	case "*":
+		totalNumber = numberOne * numberTwo
+	case "/":
+		totalNumber = numberOne / numberTwo
+	}
+
+	// returning the final number
+	return totalNumber
+
+}
+
 // function for writing to a already generated file
-func WriteProblemSheet() {
+func WriteProblemSheet(fileName string) {
+
+	// initializing the string builder
+	var builder strings.Builder
 
 	// get the file name of the math sheet
+	fmt.Println("Writing to ", fileName)
+
+	// opening the file
+	file, err := os.OpenFile(fileName, os.O_WRONLY, 0644)
+
+	// error handling
+	if err != nil {
+		panic(err)
+	}
+
+	// generating ten word problems with answers
+	for i := 0; i < 10; i++ {
+
+		// retrieving randomly generated number
+		operator := GenerateRandomOperator()
+		numberOne, numberTwo := GenerateRandomNumbers()
+
+		// retreiving the final answer
+		// answer := PerformMathOperation(numberOne, numberTwo, operator)
+
+		// building the string to write
+		builder.WriteString(strconv.Itoa(numberOne) + " ")
+		builder.WriteString(operator + " ")
+		builder.WriteString(strconv.Itoa(numberTwo) + " ")
+		builder.WriteString("= ")
+		// builder.WriteString(strconv.Itoa(answer))
+		builder.WriteString("\n")
+
+		// writing to the file
+		_, err := file.WriteString(builder.String())
+
+		// error handling
+		if err != nil {
+			panic(err)
+		}
+
+		// resetting the builder
+		builder.Reset()
+
+	}
+
+	// closing the file
+	defer file.Close()
+
+}
+
+// function for writing to a already generated file
+func WriteAnswerSheet(fileName string) {
+
+	// accessing the problem file
+	problemFile, err := os.Open(fileName)
+
+	// error handling
+	if err != nil {
+		panic(err)
+	}
+
+	// closing the file
+	defer problemFile.Close()
+
+	// accessing the answer file
+	answerFile, err := os.Open(fileName)
+
+	// error handling
+	if err != nil {
+		panic(err)
+	}
+
+	// closing the file
+	defer answerFile.Close()
+
+	// copying contents from problem to answer file
+	_, err = io.Copy(answerFile, problemFile)
+
+	// error handling
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Contents successfully copied!")
 
 }
--- Main.go
+++ Main.go
@@ -6,8 +6,17 @@ import (
 
 func main() {
 
+	// debugging message
 	fmt.Println("Running the Program")
 
-	CreateProblemSheet()
+	// creating two different sheets
+	fileNameOne, fileNameTwo := CreateSheets()
+	fmt.Println("Created ", fileNameOne)
+	fmt.Println("Created ", fileNameTwo)
+
+	// writing out the problem sheet
+	WriteProblemSheet(fileNameOne)
+	fmt.Println("Written to the Problem Sheet")
+	WriteAnswerSheet(fileNameTwo)
 
 }
